223ca2236b5afe596fc13a866e56e1891711340e
PauloViOS <pv8592@gmail.com>
2020-09-21 09:47:22 -0300

    Implemtentada Direção

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,6 +9,14 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
 
 if __name__ == '__main__':
     paulo = Pessoa(nome='Paulo')
@@ -31,3 +39,5 @@ if __name__ == '__main__':
     print(miguel.olhos)
     print(paulo.olhos)
     print(id(Pessoa.olhos), id(miguel.olhos), id(paulo.olhos))
+    print(Pessoa.metodo_estatico(), miguel.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), miguel.nome_e_atributos_de_classe())
